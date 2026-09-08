71f8fb8d0dc2b99290ddf8cefe21858682d4ae0f
ogoeli <Preciousisla235@gmail.com>
2026-09-08 15:13:06 -0700

    week 3

---
+++ WEEK3/Week3VSCode/ListCode.py
@@ -0,0 +1,42 @@
+#student list
+students = ['Alex', 'Jacob', 'Jennifer', 'David', 'Andrew', 'John', 'Kevin', 'Clay', 'Kimberly', 'Jill',
+'Joseph', 'Donitza', 'Andrew', 'Cory', 'Vincent', 'Nathan', 'Robert', 'Brandon', 'Olusegun']
+print(len(students)) #get number of items in the list
+
+#add item to end of list
+print(students.append("Joseph"))
+print(students)
+
+#add item to start of list
+print(students.insert(0,"Chris"))
+print(students)
+
+#count how mnay times an item appeared in the list
+AndCount = students.count("Andrew")
+print(AndCount)
+
+#index of 1st Andrew
+print(students.index("Andrew"))
+
+#index of 2nd Andrew
+print(students.index("Andrew", 6)) #since the 1st Andrew fell in 5th items, i seartched from 6th items till the end of the list
+
+#1st 5 items in the list
+print(students[:5])
+
+#last 5 items in the list
+print(students[16:])
+
+#remove 1st occurence of an item and place it at the end of the list
+ReAnd = str("Andrew")
+students.remove(ReAnd)
+students.append(ReAnd)
+print(students)
+
+#sort the list in ascending order
+students.sort()
+print(students)
+
+#sort the list in descending order
+students.sort(reverse=True)
+print(students)
